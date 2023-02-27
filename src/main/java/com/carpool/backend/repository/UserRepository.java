22807e35eb3c4c91b2f7a5354485e096e176ea62
Dev On Spot <114733768+team-carpool@users.noreply.github.com>
2023-02-27 16:04:59 +0530

    added @Modifying in userRepo

--- src/main/java/com/carpool/backend/repository/UserRepository.java
+++ src/main/java/com/carpool/backend/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.carpool.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -20,6 +21,7 @@ public interface UserRepository extends JpaRepository<UserModel, Long>{
 	@Query("SELECT currentLocation from UserModel um WHERE userId = :userId")
 	public String getCurrLocationByUserId(@Param(value = "userId") Long userId);
 	
+	@Modifying
 	@Query("UPDATE UserModel SET currentLocation = :loc WHERE userId = :userId")
 	public void updateLocationByUserId(@Param(value = "userId") Long userId,
 			@Param(value = "loc") String loc);
